0ff9f874a8efb00f3ea6a1d6f3f0ee393b8aeaa3
Tester <mgrican11@gmail.com>
2025-01-12 14:21:54 +0200

    2

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import requests
 
 valcode = input("Введіть валюту:")
 rate = input("Введіть дату:")
+print("-------------")
 response = requests.get(
     f"https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode={valcode}&date={rate}&json")
 
@@ -11,3 +12,4 @@ print(data[0]['txt'])
 print(data[0]['cc'])
 print(data[0]['rate'])
 print(data[0]['exchangedate'])
+print("-------------")

94dd582eea05b4505c2dae6e83302c519fb36778
LoverGreenTea <mgrican11@gmail.com>
2025-02-23 14:04:45 +0200

    2

--- Final_Project.py
+++ Final_Project.py
@@ -5,6 +5,7 @@ from PIL import Image
 from bs4 import BeautifulSoup
 from PyQt5.QtGui import QImage, QPixmap
 from urllib.request import urlopen
+from PyQt5.QtCore import QByteArray
 
 import Test3
 
@@ -19,8 +20,7 @@ GameName = QLineEdit()
 GameName.setPlaceholderText("Введіть назву ігри")
 GameCost = QLineEdit()
 GameCost.setPlaceholderText("ціна")
-GameImage = QLineEdit()
-GameImage.setPlaceholderText("Картинка")
+GameImage = QLabel()
 GameLink = QLineEdit()
 GameLink.setPlaceholderText("link")
 
@@ -31,14 +31,20 @@ main_line.addWidget(GameImage)
 main_line.addWidget(GameLink)
 main_line.addWidget(convert)
 
-
+def load_pixmap_from_url(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        pixmap = QPixmap()
+        pixmap.loadFromData(QByteArray(response.content))
+        return pixmap
+    return None
 def get_game():
     gamename = GameName.text()
     info =Test3.search_steam_game(gamename)
     GameCode_.setText("код: "+ info['appid'])
     GameCost.setText("ціна: "+ info['price'])
     GameLink.setText(info['url'])
-    GameImage.setText(info['image'])
+    GameImage.setPixmap(load_pixmap_from_url(info['image']))
 
 window.resize(300, 300)
 convert.clicked.connect(get_game)
